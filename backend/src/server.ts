4a3b6059b87dfbb3e1191c68b895ff17a63a4a51
gustavosaid <104603397+gustavosaid@users.noreply.github.com>
2025-11-25 20:10:20 +0000

    Corrigindo bind da porta para o Render

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,7 @@ import 'dotenv/config';
 
 const PORT:number = Number(process.env.PORT) || 3333; //setando a porta
 
-app.listen(PORT, (): void => {
+app.listen(PORT, '0.0.0.0', (): void => {
   console.log(`App ta rodando na porta ${PORT}`);
 });
 
